5dc2af46e486a393d29dff4ec6b0735fbd69a858
Yuriy <russian_town_98@mail.ru>
2024-05-29 01:19:54 +0600

    Faild

--- Assets/Source/Codebase/Domain/Configs/GunConfig.cs
+++ Assets/Source/Codebase/Domain/Configs/GunConfig.cs
@@ -9,6 +9,7 @@ namespace Source.Root
         [field: SerializeField] public float RecoilForce { get; private set; }
         [field: SerializeField] public float Range { get; private set; }
         [field: SerializeField] public float ReloadingSpeed { get; private set; }
+        [field: SerializeField] public int BulletCountInMagazine { get; private set; }
         [field: SerializeField] public float AccuracyFromHip { get; private set; }
         [field: SerializeField] public float AimSpeed { get; private set; }
         [field: SerializeField] public Sprite ScopeSprite { get; private set; }
